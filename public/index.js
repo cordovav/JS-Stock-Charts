ad7d0a7583f5a66673c5385abe59ff93ada8d74f
Veny Cordova <venycordova@gmail.com>
2022-10-16 12:37:06 -0400

    added twelvedata documentation

--- public/index.js
+++ public/index.js
@@ -4,6 +4,11 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+
+//created fetch request for twelvedata documentation
+    const rsponse = await fetch(`https://api.twelvedata.com/time_series?symbol=AAPL,EUR/USD,ETH/BTC:Huobi,TRP:TSX&interval=1min&apikey=3bd179df2df44c3a828326eaecc1e655`);
+
+
 }
 
 main()
